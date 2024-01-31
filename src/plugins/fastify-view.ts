50b93eb001669682ffceb173e843706362972908
Aris Ripandi <riipandi@gmail.com>
2024-02-01 01:46:48 +0700

    fix: Liquid template layout configuration

--- src/plugins/fastify-view.ts
+++ src/plugins/fastify-view.ts
@@ -4,19 +4,20 @@ import fp from "fastify-plugin";
 import { Liquid } from "liquidjs";
 
 export default fp<FastifyViewOptions>(async (fastify) => {
-  const liquidEngine = new Liquid({
-    root: path.join(__dirname, "../views"),
-    cache: process.env.NODE_ENV === "production",
-    extname: ".liquid",
-  });
-
   fastify.register(fastifyView, {
-    engine: { liquid: liquidEngine },
+    engine: {
+      liquid: new Liquid({
+        root: path.join(__dirname, "../views"),
+        layouts: resolve("src/views/layout"),
+        cache: process.env.NODE_ENV === "production",
+        extname: ".liquid",
+      }),
+    },
     viewExt: "liquid", // Sets the default extension to `.liquid`
     includeViewExtension: true,
     defaultContext: {
-      // Inside your templates, `dev` will be `true` if the expression evaluates to true
-      dev: process.env.NODE_ENV === "development",
+      // Inject some context to your templates.
+      baseUrl: "http://localhost:3000",
     },
     templates: resolve("src/views/pages"),
     options: {},
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -20,10 +20,10 @@ const root: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
     return reply.send({ pong: "it worked!" }).code(200);
   });
   fastify.get("/home", opts, async (_request, reply) => {
-    return reply.view("home", { name: "Aris Ripandi" });
+    return reply.view("home", { pageTitle: "Homepage", name: "Aris Ripandi" });
   });
   fastify.get("/about", opts, async (_request, reply) => {
-    return reply.view("about", { title: "About" });
+    return reply.view("about", { pageTitle: "About" });
   });
 };
 
